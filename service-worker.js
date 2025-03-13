869c65ba8e8b8d25a316fd634eea82b374c29c5e
realspinkyyy <spinkoliver01@gmail.com>
2025-03-13 19:14:08 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,39 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('zoom-cam').then(cache => {
-    return cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/icon.png'
-    ]);
-  }));
+const CACHE_NAME = 'zoom-cam-v3';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  '/offline.html'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
+  return self.clients.claim();
+});
+
+// Fetch with fallback
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/offline.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+  }
 });
